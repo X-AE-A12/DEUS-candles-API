29bc747e2ee180a4b0a9146eacc2c25d021cf709
nobody <timo_vvliet@live.nl>
2020-11-11 11:52:00 +0100

    Update fastify.js

--- src/fastify.js
+++ src/fastify.js
@@ -12,9 +12,8 @@ module.exports = async (routes) => {
 
     // Init server
     try {
-        console.log(process.env.PORT);
-        await fastify.listen(process.env.PORT || 8080)
-        fastify.log.info(`server listening on port ${process.env.PORT}`)
+        await fastify.listen(process.env.PORT || 8080, '0.0.0.0')
+        fastify.log.info(`server listening on ${fastify.server.address().port}`)
     } catch (err) {
         fastify.log.error(err)
         process.exit(1)
